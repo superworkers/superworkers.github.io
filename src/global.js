03a76febba7ca6df0636959cf15d280468ddc9fe
Matt Born <mattborn@users.noreply.github.com>
2025-10-29 14:12:42 -0500

    Let’s superwork!
    Refactored with nothing-cli
    
    2025-10-29 13:32:07 -0500 Refactor SVG
    2025-09-23 13:18:00 -0600 Let’s superwork!

--- src/global.js
+++ src/global.js
@@ -1,22 +1,36 @@
-//clarity.microsoft.com
-/*;(function (c, l, a, r, i, t, y) {
-  c[a] =
-    c[a] ||
-    function () {
-      ;(c[a].q = c[a].q || []).push(arguments)
-    }
-  t = l.createElement(r)
-  t.async = 1
-  t.src = 'https://www.clarity.ms/tag/' + i
-  y = l.getElementsByTagName(r)[0]
-  y.parentNode.insertBefore(t, y)
-})(window, document, 'clarity', 'script', 'NEW_CLARITY_CODE_HERE')*/
+const BRAND = 1
 
-// ScrollReveal animations
-const scrollConfig = {
-  cleanup: true,
-  distance: '20%',
-  interval: 100,
-  origin: 'bottom',
+const frames = document.querySelectorAll('#cycle img')
+let i = 0
+
+const swap = () => {
+  frames.forEach(s => s.classList.remove('active'))
+  frames[i % frames.length].classList.add('active')
+  i++
 }
-ScrollReveal().reveal('h1, h2, h3, p, a, button, img, video', scrollConfig)
+
+const cycle = () => {
+  if (i < 15) {
+    swap()
+    setTimeout(cycle, 200)
+  } else {
+    frames.forEach(s => s.classList.remove('active'))
+    frames[BRAND].classList.add('active')
+    setTimeout(() => {
+      i = 0
+      cycle()
+    }, 3000)
+  }
+}
+
+cycle()
+
+/* Tag */
+
+console.log(`███████ ██    ██ ██████  ███████ ██████  ██     ██  ██████  ██████  ██   ██ 
+██      ██    ██ ██   ██ ██      ██   ██ ██     ██ ██    ██ ██   ██ ██  ██  
+███████ ██    ██ ██████  █████   ██████  ██  █  ██ ██    ██ ██████  █████   
+     ██ ██    ██ ██      ██      ██   ██ ██ ███ ██ ██    ██ ██   ██ ██  ██  
+███████  ██████  ██      ███████ ██   ██  ███ ███   ██████  ██   ██ ██   ██
+
+We want you! Join our Discord: https://discord.gg/ng8RNjm5Jz`)
